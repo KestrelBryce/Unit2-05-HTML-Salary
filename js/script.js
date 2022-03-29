aa6fac26f4e993d29e20501df907d09065401666
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-03-29 15:36:44 +0000

    Finishing touches, it's done now

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 'use strict'
 /**
- * This function calculates area and perimeter of a triangle.
+ * This function calculates the amount of tax the Canadian government takes from your paycheck.
  */
-function calculate () {
+
+function calculate() {
   // input
-  const length = parseInt(document.getElementById('length-of-triangle').value)
-  const width = parseInt(document.getElementById('width-of-triangle').value)
+  let worked = parseInt(document.getElementById('number-hours-worked').value)
+  let rate = parseInt(document.getElementById('hourly-pay-rate').value)
 
   // process
-  const area = length * width
-  const perimeter = 2 * (length + width)
+  const TAX_RATE = 0.1805
+  let beforeTax = worked * rate 
+  let tax = TAX_RATE * beforeTax
+  let afterTax = beforeTax - tax
 
   // output
-  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
-  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+  document.getElementById('area').innerHTML = 'Your salary is $' + beforeTax.toFixed(2) + ', but the final salary that you will recieve is $' + afterTax.toFixed(2) + ". This is because the government needs your money more than you and took $" + tax.toFixed(2) + " straight out of your pocket. Spend your money before they can take it, they can't take what you don't have."
 }
